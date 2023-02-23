0ba31dd68cf132985d4bbad8867d0f13603b047e
andrewsteigerinsight <andrew.steiger@insight.com>
2023-02-23 13:16:51 -0500

    Initial Commit

---
+++ SteigerAndrewRateAPotty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SteigerAndrewRateAPotty
+//
+//  Created by asteiger on 2/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
